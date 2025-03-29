f4a7c55c78115a1206b7db633b7e71cbdcadc76a
nooblette <min9hyuk@gmail.com>
2025-03-29 14:22:51 +0900

    feat : 로딩 스피너 수정

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -54,7 +54,7 @@ const OrderForm = () => {
   // 로딩 스피너
   const LoadingSpinner = () => (
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-orange-400"></div>
+      <div className="inline-block animate-spin rounded-full h-12 w-12 border-4 border-orange-300 border-t-orange-500 mb-4"></div>
     </div>
   );
 
